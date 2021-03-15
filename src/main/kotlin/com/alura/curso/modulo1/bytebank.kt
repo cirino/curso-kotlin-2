d623eee5dd1c3910e311a75229452cb9dd15338d
Dagmar Cirino <dagmar.mv@gmail.com>
2021-03-15 04:59:19 -0300

    Coportamentos de classes

--- src/main/kotlin/com/alura/curso/modulo1/bytebank.kt
+++ src/main/kotlin/com/alura/curso/modulo1/bytebank.kt
@@ -1,8 +1,25 @@
 package com.alura.curso.modulo1
 
 fun main() {
-	testaComportamentosConta()
-//	testaLacos()
+	val cirino = Funcionario(
+		"Cirino",
+		"111,1,1,1",
+		1000.0
+	)
+
+	println("Funcionario: ${cirino.nome}")
+	println(cirino.bonificacao())
+
+	val dagmar = Gerente(
+		"Dagmar",
+		"122.33.44.5",
+		2000.0,
+		1234
+	)
+
+	println("Gerente: ${dagmar.nome}")
+	println(dagmar.bonificacao())
+	println(dagmar.autentica())
 }
 
 
